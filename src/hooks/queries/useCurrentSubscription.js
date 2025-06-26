82e91726b56785b52597e90b50b51afad863c196
AbdulrahmanAhmedZaghloul <abdozaghloul990@gmail.com>
2025-06-26 16:08:52 +0300

    'alertmessage'

--- src/hooks/queries/useCurrentSubscription.js
+++ src/hooks/queries/useCurrentSubscription.js
@@ -1,12 +1,20 @@
+// hooks/queries/useCurrentSubscription.js
 import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '../../api/config';
 import { useAuth } from '../../context/authContext';
 
 const fetchCurrentSubscription = async (token) => {
-    const { data } = await axiosInstance.get("stripe/subscriptions/plans", {
+
+    const { data } = await axiosInstance.get("stripe/products", {
         headers: { Authorization: `Bearer ${token}` },
     });
+
+    // نفترض أن أول عنصر في المصفوفة هو الخطة الحالية
+  
+    console.log(data);
+
     return data;
+
 };
 
 export const useCurrentSubscription = () => {
@@ -15,8 +23,8 @@ export const useCurrentSubscription = () => {
     return useQuery({
         queryKey: ['currentSubscription'],
         queryFn: () => fetchCurrentSubscription(user.token),
-        staleTime: 1000 * 60 * 5, // 5 minutes
-        cacheTime: 1000 * 60 * 10, // 10 minutes
+        staleTime: 1000 * 60 * 5, // 5 دقائق
+        cacheTime: 1000 * 60 * 10, // 10 دقائق
         enabled: !!user?.token,
     });
-};
+};  
